63394421a183be89fcb732f462334d87db327873
khaing211 <khaing211@gmail.com>
2014-12-14 19:56:31 -0800

    Add the first unit test for Expr.g4 and compile flow

--- buildSrc/src/main/groovy/com/github/appocalypse/build/antlr4/Antlr4Transpile.groovy
+++ buildSrc/src/main/groovy/com/github/appocalypse/build/antlr4/Antlr4Transpile.groovy
@@ -1,45 +1,97 @@
 package com.github.appocalypse.build.antlr4
 
-import org.gradle.api.DefaultTask
+import java.io.File;
+
 import org.gradle.api.file.FileCollection
 import org.gradle.api.tasks.Input
-import org.gradle.api.tasks.Optional;
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.SkipWhenEmpty
+import org.gradle.api.tasks.SourceTask
 import org.gradle.api.tasks.TaskAction
 
-class Antlr4Transpile extends DefaultTask {
 
-	@Input
-	@SkipWhenEmpty
-	FileCollection source
+class Antlr4Transpile extends SourceTask {
 	
 	@Input
 	Object antlr4Classpath
 	
 	@Input
 	@Optional
-	String packageNamespace	
+	String javaPackage
 	
 	@Input
 	@Optional
-	boolean noListener = false
+	boolean generateListener = false
 	
 	@Input
 	@Optional
-	boolean noVisitor = false
+	boolean generateVisitor = false
 	
 	@Input
 	@Optional
-	boolean Werror = false
+	boolean treatWarningAsError = false
+	
+	@Input
+	@Optional
+	boolean showLongMessages = false
+	
+	@Input
+	@Optional
+	String grammarFileEncoding
+	
+	@OutputDirectory
+	File destinationDir = getTemporaryDir()
+	
+	Antlr4Transpile() {
+		include('**/*.g4')	
+	}
 	
 	@TaskAction
 	void transpile() {
+		final def destinationDirPath = getDestinationDir().canonicalPath
+		final def packagePath = javaPackage ? javaPackage.replaceAll("\\.", "/") : ""
+		final def classPath = "${destinationDirPath}/${packagePath}"
+		
+		final def antlr4Args = []
+		
+		if (generateListener) {
+			antlr4Args << '-listener'
+		} else {
+			antlr4Args << '-no-listener'
+		}
+		
+		if (generateVisitor) {
+			antlr4Args << '-visitor'
+		} else {
+			antlr4Args << '-no-visitor'
+		}
+		
+		if (treatWarningAsError) {
+			antlr4Args << '-Werror'
+		}
+		
+		if (showLongMessages) {
+			antlr4Args << '-long-messages'
+		}
+		
+		if (grammarFileEncoding) {
+			antlr4Args << ['-encoding', grammarFileEncoding]
+		}
+		
+		antlr4Args << ['-o', classPath]
+		
+		if (javaPackage) {
+			antlr4Args << ['-package', javaPackage]
+		}
+		
+		antlr4Args << getSource().files
+		
 		getProject().javaexec {
 			classpath = antlr4Classpath
 			main = 'org.antlr.v4.Tool'
-		}
-		
+			
+			args = antlr4Args.flatten()
+		}		
 	}
-
-
 }
