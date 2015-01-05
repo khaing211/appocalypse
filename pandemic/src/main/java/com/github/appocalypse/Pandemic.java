5d047a4218a588669482ffe42aa1cce5ddefc995
khaing211 <khaing211@gmail.com>
2015-01-05 11:08:07 -0800

    Complete jline simple echo loop

--- pandemic/src/main/java/com/github/appocalypse/Pandemic.java
+++ pandemic/src/main/java/com/github/appocalypse/Pandemic.java
@@ -1,6 +1,11 @@
 package com.github.appocalypse;
 
+import java.io.PrintWriter;
+import java.util.LinkedList;
+import java.util.List;
+
 import jline.console.ConsoleReader;
+import jline.console.completer.Completer;
 
 public class Pandemic {
 
@@ -8,6 +13,19 @@ public class Pandemic {
 		final ConsoleReader reader = new ConsoleReader();
 		reader.setPrompt("pandemic> ");
 		
+		final List<Completer> completors = new LinkedList<Completer>();
+		
+		completors.forEach(completer -> reader.addCompleter(completer));
+		
+		final PrintWriter out = new PrintWriter(reader.getOutput());
+		
+		String line = null;
+		while ((line = reader.readLine()) != null) {
+			
+			out.println(line);
+			
+			out.flush();
+		}
 	}
 
 }
