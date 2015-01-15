e6a2181c21f9e82e6012a73c36a0e1e13f125448
khaing211 <khaing211@gmail.com>
2015-01-15 07:10:15 -0800

    Add more skeleton code for event and completer

---
+++ pandemic/src/main/java/com/github/appocalypse/pandemic/event/TreatEvent.java
@@ -0,0 +1,13 @@
+package com.github.appocalypse.pandemic.event;
+
+import com.github.appocalypse.pandemic.Scenario;
+
+public class TreatEvent implements Event {
+
+	@Override
+	public Scenario actUpon(Scenario scenario) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ pandemic/src/main/java/com/github/appocalypse/pandemic/parser/PandemicEventParser.java
@@ -0,0 +1,11 @@
+package com.github.appocalypse.pandemic.parser;
+
+public class PandemicEventParser {
+	public static interface TokenEater {
+		
+	}
+	
+	public TokenEater parse(String line) {
+		return null;
+	}
+}
