48f7ad6d28a80d991dc35a8c19f05933b6580523
khaing211 <khaing211@gmail.com>
2015-01-16 07:25:04 -0800

    Move some code to GoogleService

--- optionzero/src/main/java/com/github/appocalypse/OptionZero.java
+++ optionzero/src/main/java/com/github/appocalypse/OptionZero.java
@@ -1,68 +1,44 @@
 package com.github.appocalypse;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.List;
 
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
-import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.auth.oauth2.GoogleOAuthConstants;
-import com.google.api.client.googleapis.auth.oauth2.GoogleTokenResponse;
-import com.google.api.client.http.HttpTransport;
-import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.jackson2.JacksonFactory;
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.CommandLineParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.PosixParser;
+
+import com.github.appocalypse.optionzero.GoogleService;
 import com.google.api.services.gmail.Gmail;
 import com.google.api.services.gmail.model.ListThreadsResponse;
 import com.google.api.services.gmail.model.Thread;
 
 public class OptionZero {
 	
-	// Check https://developers.google.com/gmail/api/auth/scopes for all
-	// available scopes
-	private static final String SCOPE = "https://www.googleapis.com/auth/gmail.readonly";
-	private static final String APP_NAME = "Option Zero";
 	// Email address of the user, or "me" can be used to represent the currently
 	// authorized user.
 	private static final String USER = "me";
 
 	public static void main(String[] args) throws Exception {
-		HttpTransport httpTransport = new NetHttpTransport();
-		JsonFactory jsonFactory = new JacksonFactory();
+		Options options = new Options();
+		
+		options.addOption("h", "help", false, "print this message");
+
+		CommandLineParser parser = new PosixParser();
+		CommandLine cmd = parser.parse( options, args);
+		
+		if (cmd.hasOption("h")) {
+			HelpFormatter formatter = new HelpFormatter();
+			formatter.printHelp( "OptionZero", options );
+			return;
+		}
+		
 
 		final String clientSecretPath = args[0];
 		
-		GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(jsonFactory, 
-				new FileReader(clientSecretPath));
-
-		// Allow user to authorize via url.
-		GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-				httpTransport, jsonFactory, clientSecrets, Arrays.asList(SCOPE))
-				.setAccessType("online").setApprovalPrompt("auto").build();
-
-		String url = flow.newAuthorizationUrl().setRedirectUri(GoogleOAuthConstants.OOB_REDIRECT_URI).build();
-		
-		System.out.println("Open your web browser and authorize " + url);
-		System.out.println("Enter the code: ");
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String code = br.readLine();
-
-		// Generate Credential using retrieved code.
-		GoogleTokenResponse response = flow.newTokenRequest(code)
-				.setRedirectUri(GoogleOAuthConstants.OOB_REDIRECT_URI)
-				.execute();
-		
-		GoogleCredential credential = new GoogleCredential()
-				.setFromTokenResponse(response);
-
 		// Create a new authorized Gmail API client
-		Gmail service = new Gmail.Builder(httpTransport, jsonFactory,
-				credential).setApplicationName(APP_NAME).build();
-
+		Gmail service = new GoogleService().connectToGmail(clientSecretPath, "user", "pass");
+		
 		// Retrieve a page of Threads; max of 100 by default.
 		ListThreadsResponse threadsResponse = service.users().threads()
 				.list(USER).execute();
