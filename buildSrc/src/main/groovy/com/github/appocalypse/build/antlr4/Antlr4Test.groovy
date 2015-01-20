60f9903141b4c92a83bd5eb882cc9e7cfeb03414
khaing211 <khaing211@gmail.com>
2015-01-20 06:48:39 -0800

    Adding more parameter to antlr4 test task

--- buildSrc/src/main/groovy/com/github/appocalypse/build/antlr4/Antlr4Test.groovy
+++ buildSrc/src/main/groovy/com/github/appocalypse/build/antlr4/Antlr4Test.groovy
@@ -1,15 +1,38 @@
 package com.github.appocalypse.build.antlr4
 
 import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.Input
 import org.gradle.api.tasks.InputFiles
-import org.gradle.api.tasks.TaskAction;
+import org.gradle.api.tasks.Optional
+import org.gradle.api.tasks.TaskAction
 
 /**
  * Test Java compile of grammars code against example files
  */
 class Antlr4Test extends DefaultTask {
+	@Input
+	Object antlr4Classpath
+	
 	@InputFiles
 	def exampleFiles
+	
+	@Input
+	@Optional
+	def packageName
+	
+	@Input
+	def grammarName
+	
+	@Input
+	@Optional
+	def showTrees
+	
+	@Input
+	@Optional
+	def verbose
+	
+	@Input
+	def entryPoint
 	
 	@TaskAction
 	void antlr4Test() {
