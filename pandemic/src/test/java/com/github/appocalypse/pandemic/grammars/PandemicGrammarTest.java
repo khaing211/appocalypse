ab984a2580af85e60e8380db60a019e8a58a203e
khaing211 <khaing211@gmail.com>
2015-01-31 20:43:36 -0800

    Play around with antlr4 for token suggestion

--- pandemic/src/test/java/com/github/appocalypse/pandemic/grammars/PandemicGrammarTest.java
+++ pandemic/src/test/java/com/github/appocalypse/pandemic/grammars/PandemicGrammarTest.java
@@ -2,13 +2,12 @@ package com.github.appocalypse.pandemic.grammars;
 
 import org.antlr.v4.runtime.ANTLRInputStream;
 import org.antlr.v4.runtime.CommonTokenStream;
-import org.antlr.v4.runtime.ParserRuleContext;
 import org.antlr.v4.runtime.tree.ErrorNode;
 import org.antlr.v4.runtime.tree.TerminalNode;
 import org.junit.Test;
 
+import com.github.appocalypse.guava.extra.GuavaCollectors;
 import com.github.appocalypse.pandemic.grammars.PandemicParser.ActionContext;
-import com.github.appocalypse.pandemic.grammars.PandemicParser.ActionsContext;
 import com.github.appocalypse.pandemic.grammars.PandemicParser.BuildActionContext;
 import com.github.appocalypse.pandemic.grammars.PandemicParser.CharterFlightActionContext;
 import com.github.appocalypse.pandemic.grammars.PandemicParser.CityContext;
@@ -26,183 +25,117 @@ public class PandemicGrammarTest {
 
 	@Test
 	public void test() {
-		ANTLRInputStream antlrInputStream = new ANTLRInputStream("discover a cure by discard Ho Chi Minh City"
-				+ " , Atlanta , Washington , Chicag");
+		final String buffer = "discover a cure by discard Ho Chi Minh City , Atlanta , Washington , Chicag";
+		
+		ANTLRInputStream antlrInputStream = new ANTLRInputStream(buffer);
 		
 		PandemicLexer lexer = new PandemicLexer(antlrInputStream);
 		CommonTokenStream commonsTokenStream = new CommonTokenStream(lexer);
 		PandemicParser parser = new PandemicParser(commonsTokenStream);
-		parser.addParseListener(new PandemicListener() {
+		parser.addParseListener(new PandemicBaseListener() {
 
 			@Override
 			public void visitTerminal(TerminalNode node) {
 				System.out.println("visitTerminal");
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				printTokens(parser);
 			}
 
 			@Override
 			public void visitErrorNode(ErrorNode node) {
 				System.out.println("visitErrorNode");
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void enterEveryRule(ParserRuleContext ctx) {
-				System.out.println("enterEveryRule");
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitEveryRule(ParserRuleContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterRole(RoleContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitRole(RoleContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterRole");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterDirectFlightAction(DirectFlightActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitDirectFlightAction(DirectFlightActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterDirectFlightAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterCity(CityContext ctx) {
 				System.out.println("enterCity");
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitCity(CityContext ctx) {
-				System.out.println("exitCity");
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterCharterFlightAction(CharterFlightActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitCharterFlightAction(CharterFlightActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterDiscoverAction(DiscoverActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitDiscoverAction(DiscoverActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterDiscoverAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterFerryAction(FerryActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitFerryAction(FerryActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterFerryAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterQuitAction(QuitActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitQuitAction(QuitActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterQuitAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterJoinAction(JoinActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitJoinAction(JoinActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterJoinAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterBuildAction(BuildActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitBuildAction(BuildActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterBuildAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterAction(ActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitAction(ActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void enterActions(ActionsContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitActions(ActionsContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterOperationFlightAction(
 					OperationFlightActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitOperationFlightAction(
-					OperationFlightActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterOperationFlightAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterTreatAction(TreatActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
-
-			@Override
-			public void exitTreatAction(TreatActionContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterTreatAction");
+				printTokens(parser);
 			}
 
 			@Override
 			public void enterPlayer(PlayerContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
+				System.out.println("enterPlayer");
+				printTokens(parser);
 			}
 
-			@Override
-			public void exitPlayer(PlayerContext ctx) {
-				System.out.println(parser.getExpectedTokensWithinCurrentRule().toList());				
-			}
 		});
 		
-		parser.actions();
+		parser.action();
 	}
 
+	private void printTokens(PandemicParser parser) {
+		parser.getExpectedTokensWithinCurrentRule()
+			.toList()
+			.stream()
+			.filter(i -> i >= 0)
+			.map(i -> parser.tokenNames[i])
+			.peek(System.out::println)
+			.collect(GuavaCollectors.toImmutableList());
+	}
 }
