f99d80740d4a8d7c52be63915741e0bce9495764
khaing211 <khaing211@gmail.com>
2015-02-04 17:28:21 -0800

    Add skeleton OsgiProjectPlugin class to implement convention for osgi project

---
+++ buildSrc/src/main/groovy/com/github/appocalypse/build/osgi/OsgiProjectPlugin.groovy
@@ -0,0 +1,13 @@
+package com.github.appocalypse.build.osgi
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class OsgiProjectPlugin implements Plugin<Project> {
+
+	@Override
+	public void apply(Project project) {
+		// TODO Auto-generated method stub
+		
+	}
+}
