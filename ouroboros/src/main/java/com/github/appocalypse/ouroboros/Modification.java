a71934314681d34d42d17a61a8c92cf76e7c92d5
khaing211 <khaing211@gmail.com>
2015-04-29 06:57:03 -0700

    Create two interface in ouroboros

---
+++ ouroboros/src/main/java/com/github/appocalypse/ouroboros/Modification.java
@@ -0,0 +1,4 @@
+package com.github.appocalypse.ouroboros;
+
+public interface Modification {
+}
---
+++ ouroboros/src/main/java/com/github/appocalypse/ouroboros/SelfUpdate.java
@@ -0,0 +1,4 @@
+package com.github.appocalypse.ouroboros;
+
+public interface SelfUpdate<T extends Modification> {
+}
