04ef084eb54a62cea67897a20dcc063a0f3046d0
khaing211 <khaing211@gmail.com>
2015-06-14 14:11:20 -0700

    Remove jdk-extra, guava-extra Add boilerpipe

--- jdk-extra/src/main/java/com/github/appcalypse/jdk/extra/comparator/JdkComparators.java
+++
@@ -1,9 +0,0 @@
-package com.github.appcalypse.jdk.extra.comparator;
-
-import java.util.Comparator;
-
-public interface JdkComparators {
-	public static <T> Comparator<T> allIdentical() {
-		return (i, j) -> 0;
-	}
-}
